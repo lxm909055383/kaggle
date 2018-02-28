eb24767336caf0a56a7de0c5611b01a750929798
lxm <909055383@qq.com>
2018-02-28 13:27:58 +0800

    ok

---
+++ toxic_comment_classification/commentClassification.py
@@ -0,0 +1,7 @@
+
+import pandas as pd
+import numpy as np
+trainContent = pd.read_csv('train.csv')
+testContent = pd.read_csv('test.csv')
+sampleContent = pd.read_csv('sample_submission.csv')
+print(testContent)
