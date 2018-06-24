6bdff4191beda58978c21a395a890a553de16c0a
lxm909055383 <909055383@qq.com>
2018-06-24 17:32:51 +0800

    fix

--- toxic_comment_classification/lxmlll.py
+++ toxic_comment_classification/lxmlll.py
@@ -1,22 +1,12 @@
-#!/usr/bin/python
-#coding:utf-8
+from textblob import TextBlob
+import nltk
 
-import pandas as pd
-import numpy as np
-import re
-import pandas as pd
-import numpy as np
-import re
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-#
-# print(r"\n")
-text = "Why the edits made under my username Hardcore Metallica Fan were reverted? They weren't vandalisms, just closure on some GAs after I voted at New York Dolls FAC. And please don't remove the template from the talk page since I'm retired now.89.205.38.27"
-text = re.sub(r"[a-zA-Z]*'[a-zA-Z]*", "", text)
-text_vector = TfidfVectorizer()
-text_vector.fit(text)
-train_vec = text_vector.transform(text)
-# text = re.sub(r"[^A-Za-z]", " ", text)  #正则替换
-print(train_vec)
+# from nltk.corpus import treebank
+# t = treebank.parsed_sents('wsj_0001.mrg')[0]
+# print(t.draw())
+
+# nltk.download('averaged_perceptron_tagger')
+
+
+wiki = TextBlob("Python is a high-level, general-purpose programming language.")
+print(wiki.tags)
